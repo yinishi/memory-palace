3bbad7f2c44aa3390ccfd3b3def5b3934ca67b5d
Svinci131 <svinci131@gmail.com>
2016-06-22 17:42:49 -0400

    we have a flooooooooor

---
+++ js/constants.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const constants = {
+	WIDTH : window.innerWidth,
+	HEIGHT : window.innerHeight,
+	// ASPECT : constants.WIDTH / constants.HEIGHT,
+	UNITSIZE : 250,
+	// WALLHEIGHT : constants.UNITSIZE / 3
+}
+module.exports = constants;
--- js/index.js
+++ js/index.js
@@ -1,1 +1,32 @@
-console.log("hi")
+'use strict'
+// console.log("hi")
+const WIDTH = require("./constants").WIDTH;
+const HEIGHT = require("./constants").HEIGHT;
+const ASPECT = WIDTH / HEIGHT;
+const UNITSIZE = require("./constants").UNITSIZE;
+// const MOVESPEED = 100;
+// const LOOKSPEED = 0.075;
+
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera( 60, ASPECT, 1, 10000 );
+
+camera.position.set( 0, 0, UNITSIZE*.2);
+scene.add(camera);
+
+let renderer = require("./renderer"); 
+
+let floorGeometry = new THREE.PlaneBufferGeometry( 30, 30);
+let floorMaterial = new THREE.MeshBasicMaterial({ color: 0xcccccc});
+// floorGeometry.rotateX( - Math.PI / 2 );
+
+let floor = new THREE.Mesh(floorGeometry, floorMaterial);
+floor.rotation.set( 2, -Math.PI / 2 - 1.7, 2 );
+floor.position.set(0, -5, 0)
+
+scene.add(floor);
+
+
+renderer.render(scene, camera);
+var container = document.createElement( 'div' );
+document.body.appendChild( container );
+container.appendChild( renderer.domElement );
