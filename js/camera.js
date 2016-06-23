d286f18b65174b685e66025735645349814332e3
Svinci131 <svinci131@gmail.com>
2016-06-23 13:41:07 -0400

    add position and roation controlls

--- js/camera.js
+++ js/camera.js
@@ -1,16 +1,82 @@
 const WIDTH = require("./constants").WIDTH;
 const HEIGHT = require("./constants").HEIGHT;
 const ASPECT = WIDTH / HEIGHT;
-//fov — Camera frustum vertical field of view.
-//aspect — Camera frustum aspect ratio.
-//near — Camera frustum near plane.
-//far — Camera frustum far plane.
+let PI = Math.PI;
+
+let positionX = 0;
+let positionY = 0;
+let positionZ = 100;
+
+let rotationX = 0;
+let rotationY = 0;
+let rotationZ = 0;
+let fiveDegrees = (((PI/2)/3)/3)/2;
+
 
 let camera = new THREE.PerspectiveCamera(60, ASPECT, 1, 10000);
 
+camera.position.set(positionX, positionY, positionZ);
+camera.rotation.set(rotationX, rotationY, rotationZ);
 
-camera.position.set(0, 0, 80);
-// camera.rotation.set(floorX, floorY, floorZ);
+function getCamera() {
+	return camera;
+}
+function moveLeft() {
+	camera.position.x-=3;
+}
+function moveRight() {
+	camera.position.x+=3;
+}
+function moveUp() {
+	camera.position.y+=3;
+}
+function moveDown() {
+	camera.position.y-=3;
+}
+function lookDown() {
+	camera.rotation.x-=fiveDegrees;
+}
+function lookUp() {
+	camera.rotation.x+=fiveDegrees;
+}
+function lookLeft() {
+	camera.rotation.y+=fiveDegrees;
+}
+function lookRight() {
+	camera.rotation.y-=fiveDegrees;
+}
+function moveForward(){
+	camera.position.z-=3;
+}
+function moveBackward(){
+	camera.position.z+=3;
+}
+//zoomout- increasing position z 
+//zoomin- decreasing position z 
+
+//move right //increasing x
+
+//lookingleft //increase why
+//look right 
+//lookup //decrease z
+//look down //increase z
 
 
-module.exports = camera;
+module.exports = {
+	getCamera: getCamera,
+	moveLeft: moveLeft,
+	moveRight: moveRight,
+	moveUp: moveUp,
+	moveDown: moveDown, 
+	lookDown: lookDown,
+	lookLeft: lookLeft,
+	lookRight: lookRight,
+	lookUp: lookUp,
+	moveForward: moveForward,
+	moveBackward: moveBackward
+};
+
+
+
+
+
--- js/index.js
+++ js/index.js
@@ -8,9 +8,9 @@ const UNITSIZE = require("./constants").UNITSIZE;
 // CONSTRUCTOR FUNCTIONS
 const Room = require('./constructors/RoomConstructor.js')
 const Table = require('./constructors/TableConstructor.js')
-
+const cameraControls = require('./camera.js');
 // REQUIRING OBJECTS
-const camera = require('./camera.js')
+const camera = cameraControls.getCamera();
 
 
 // CREATING SCENE
@@ -19,11 +19,10 @@ const scene = new THREE.Scene();
 scene.add(camera);
 // // CREATE A TABLE
 let table = new Table().mesh
-console.log(table)
 
 // CREATE A ROOM
 let room = new Room().mesh
-const roomRotationX = - Math.PI / 2 + 0.5
+const roomRotationX = - Math.PI / 2 
 const roomRotationY = 0
 const roomRotationZ = -0.3
 room.rotation.set(roomRotationX, roomRotationY, roomRotationZ)
@@ -37,10 +36,8 @@ scene.add(room)
 
 var geomCube = new THREE.BoxGeometry(4, 4, 4);
 var matCube = new THREE.MeshBasicMaterial({color: 0x000000})
-var cube = new THREE.Mesh(geomCube, matCube)
-
-// room.add(cube)
-
+var cube = new THREE.Mesh(geomCube, matCube);
+ 
 table.position.set(0, -10, 6)
 room.add(table)
 
@@ -53,3 +50,65 @@ renderer.render(scene, camera);
 var container = document.createElement('div');
 document.body.appendChild(container);
 container.appendChild(renderer.domElement);
+
+// CONTROLLS
+window.addEventListener('wheel', e => wheelEvents(e));
+
+function wheelEvents(event){
+	if(event.deltaY > 0){
+		cameraControls.moveForward();
+		renderer.render(scene, camera);
+	}else{
+		cameraControls.moveBackward();
+		renderer.render(scene, camera);
+	}
+}
+ 
+$("body").keydown(function(e) {
+	if(e.keyCode === 37) { //left
+		cameraControls.moveLeft();
+		renderer.render(scene, camera)
+	}
+	else if(e.keyCode === 39) { //right
+		cameraControls.moveRight();
+		renderer.render(scene, camera)
+	}
+	else if(e.keyCode === 38) { //up
+		cameraControls.moveUp();
+		renderer.render(scene, camera)
+	}
+	else if(e.keyCode === 40) { //down
+		cameraControls.moveDown();
+		renderer.render(scene, camera)
+	}
+	//look up //c
+	else if (e.keyCode === 67) {
+		cameraControls.lookUp();
+		renderer.render(scene, camera);
+	}
+	//look down //x
+	else if (e.keyCode === 88) {
+		cameraControls.lookDown();
+		renderer.render(scene, camera);
+	}
+	//look left //z
+	else if (e.keyCode === 90) {
+		cameraControls.lookLeft();
+		renderer.render(scene, camera);
+	}
+	//look right //v
+	else if (e.keyCode === 86) {
+		cameraControls.lookRight();
+		renderer.render(scene, camera);
+	}
+
+});
+
+//resize image to fit screen
+window.addEventListener( 'resize', onWindowResize, false );
+
+function onWindowResize() {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+}
