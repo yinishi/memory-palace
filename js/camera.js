c437a40b82636e28f56d246583b40c94a1a32528
Svinci131 <svinci131@gmail.com>
2016-06-23 11:46:22 -0400

    init

--- js/camera.js
+++ js/camera.js
@@ -1,6 +1,10 @@
 const WIDTH = require("./constants").WIDTH;
 const HEIGHT = require("./constants").HEIGHT;
 const ASPECT = WIDTH / HEIGHT;
+//fov — Camera frustum vertical field of view.
+//aspect — Camera frustum aspect ratio.
+//near — Camera frustum near plane.
+//far — Camera frustum far plane.
 
 let camera = new THREE.PerspectiveCamera(60, ASPECT, 1, 10000);
 
