d9a60c678554e51107fe9e8fa8a6efbcedae0cce
Samantha Vinci <svinci131@gmail.com>
2016-06-27 10:54:38 -0400

    remove comments

--- server/db/index.js
+++ server/db/index.js
@@ -7,24 +7,3 @@ var Item = require('./models/Item.js');
 
 Item.belongsTo(User);
 module.exports = db;
-
-///getloggedinuser
-// app.directive('navbar', function (Auth, $state, $location) {
-//   return {
-//     restrict: 'E',
-//     templateUrl: '/browser/components/navbar/navbar.html',
-//     link: function (scope) {
-//       scope.pathStartsWithStatePath = function (state) {
-//         var partial = $state.href(state);
-//         var path = $location.path();
-//         return path.startsWith(partial);
-//       };
-//       scope.logout = function () {
-//         return Auth.logout()
-//         .then(function () {
-//           $state.go('home');
-//         });
-//       }
-//     }
-//   }
-// });
