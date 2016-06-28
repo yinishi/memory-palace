99ef361cbf0cd590419ed79f7c5584e9e8939fb0
Svinci131 <svinci131@gmail.com>
2016-06-28 19:32:42 -0400

    carousel

--- browser/js/controllers/menuCtrl.js
+++ browser/js/controllers/menuCtrl.js
@@ -6,18 +6,21 @@
     objectFactory.setCurrentObject(objectName);
   };
  	
-  $scope.slickConfig = {
-    enabled: true,
-    autoplay: false,
-    draggable: false,  
-    autoplaySpeed: 3000,
-    method: {},
-    event: {
-	        beforeChange: function (event, slick, currentSlide, nextSlide) {
-	        },
-	        afterChange: function (event, slick, currentSlide, nextSlide) {
-	        }
-	    }
-	};
+ //  $scope.slickConfig = {
+ //    enabled: true,
+ //    autoplay: false,
+ //    draggable: false, 
+ //    centerMode: true, 
+ //    adaptiveHeight: true,
+ //    autoplaySpeed: 3000,
+ //    method: {},
+ //    event: {
+	//         beforeChange: function (event, slick, currentSlide, nextSlide) {
+	//            console.log(event)
+ //          },
+	//         afterChange: function (event, slick, currentSlide, nextSlide) {
+	//         }
+	//     }
+	// };
   
 };
