081461eb8571d0338d395c3acbb67ee4957ea348
Yini Shi <yini.shi@gmail.com>
2016-06-29 18:05:02 -0400

    fixed controller issue and fixed missing sky scripts

--- browser/js/controllers/index.js
+++ browser/js/controllers/index.js
@@ -1,5 +1,4 @@
 var app = require('angular').module('memoryPalace');
 
-app.controller('room', ["$scope", require("./room_cntrl.js")]);
 app.controller('login', ["$scope", "$state", "$http", "authFactory", '$rootScope', require("./loginCtrl.js")]);
 app.controller('menuCtrl', ["$scope","objectFactory", require("./menuCtrl.js")]);
