8a43f44db9c6f861ca405c6f3581a35137459d87
borisonr <borisonr@gmail.com>
2016-07-05 15:31:34 -0400

    got rid of states

--- browser/js/states/index.js
+++ browser/js/states/index.js
@@ -1,23 +1,9 @@
 var app = require('angular').module('memoryPalace');
 
 app.config(['$stateProvider', function ($stateProvider) {
-    $stateProvider.state("about", {
-    	url: "/about",
-        templateUrl: '/browser/js/templates/about.html',
-    })
-    .state("room", {
+    $stateProvider.state("room", {
     	url: "/room",
         templateUrl: '/browser/js/templates/room.html',
     })
-    .state("login", {
-    	url: "/login",
-        templateUrl: '/browser/js/templates/login.html',
-        controller: 'login'
-    })
-    .state("signup", {
-        url: "/signup",
-        templateUrl: '/browser/js/templates/signup.html',
-        controller: 'login'
-    });
 }]);
 
